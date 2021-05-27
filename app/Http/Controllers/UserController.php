a2b37f14182eb8dbeeccc3b04d49ccf2da78420c
Jorge Valdés <32805269+jorgev121@users.noreply.github.com>
2021-05-27 15:35:49 -0600

    Agregado manejo básico de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
 
 class UserController extends Controller
 {
@@ -14,8 +16,92 @@ class UserController extends Controller
      * @param  \App\Models\User  $model
      * @return \Illuminate\View\View
      */
-    public function index(User $model)
+    public function index()
     {
-        return view('users.index', ['users' => $model->paginate(15)]);
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $user = new User;
+        $user->name             = $request->name;
+        $user->email            = $request->email;
+        $user->password         = bcrypt($request->password);
+        $user->save();
+
+        return redirect()->route('user.index') ->with('success','Registro creado correctamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name             = $request->name;
+        $user->email            = $request->email;
+        $user->password         = bcrypt($request->password);
+        $user->save();
+
+        return redirect()->route('user.index') ->with('success','Registro editado correctamente');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+        return redirect()->route('user.index') ->with('success','Registro eliminado correctamente');
     }
 }
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app', ['activePage' => 'producto', 'titlePage' => __('Crear Producto')])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card ">
+                    <div class="card-header card-header-rose card-header-icon">
+                        <div class="card-icon">
+                            <i class="material-icons">control_point_duplicate</i>
+                        </div>
+                        <h4 class="card-title">Agregar Usuario</h4>
+                    </div>
+                    <form method="POST" action="{{ route('user.store') }}">
+                        <div class="card-body ">
+                            @csrf
+                            <div class="row">
+                                <div class="form-group bmd-form-group col-md-12">
+                                    <label for="name" class="bmd-label-floating">Nombre</label>
+                                    <input required type="text" class="form-control" id="name" name="name">
+                                </div>
+                                <div class="form-group bmd-form-group col-md-6">
+                                    <label for="email" class="bmd-label-floating">Correo</label>
+                                    <input required type="email" class="form-control" id="email" name="email">
+                                </div>
+                                <div class="form-group bmd-form-group col-md-6">
+                                    <label for="password" class="bmd-label-floating">Contraseña</label>
+                                    <input required type="password" class="form-control" id="password" name="password">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer ">
+                            <a href="{{ route('user.index') }}"><button type="button" class="btn btn-fill btn-default">Cancelar</button></a>
+                            <button type="submit" class="btn btn-fill btn-rose">Agregar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
